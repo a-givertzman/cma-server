6728a68d5d08c3ebd1a53704a82ac750679b8cef
Anton Lobanov <a.givertzman@icloud.com>
2025-07-17 13:36:54 +0300

    FrdmService | RopeDeprecationRate + test

--- src/tests/unit/services/frdm_service/rope_slices_test.rs
+++ src/tests/unit/services/frdm_service/rope_slices_test.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
 use std::cell::RefCell;
-use std::{sync::Once, time::{Duration, Instant}};
+use std::{rc::Rc, sync::Once, time::{Duration, Instant}};
 use sal_core::dbg::Dbg;
-use sal_sync::services::{conf::ConfTree, entity::ToPoint};
+use sal_sync::{math::AproxEq, services::{conf::ConfTree, entity::ToPoint}};
 use testing::stuff::max_test_duration::TestDuration;
 use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
 use crate::services::{RopeConf, RopeSlices};
@@ -35,32 +35,33 @@ fn new() {
     test_duration.run().unwrap();
     let test_data = [
         //    pos         load    
-        (01,  Some(0.50),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (02,  None      ,  Some(1.0),      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (03,  Some(0.51),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (04,  Some(0.52),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (05,  Some(0.53),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (06,  Some(0.54),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (07,  Some(0.55),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (08,  Some(0.56),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (09,  Some(0.57),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (10,  Some(0.58),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (11,  Some(0.59),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (12,  Some(0.60),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.61),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.62),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.63),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.64),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.65),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.66),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.67),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.68),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.69),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.70),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.71),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
-        (13,  Some(0.72),  None     ,      vec![(0, 0.0), (0, 0.0), (0, 0.0)]),
+        (01,  Some(0.50),  None     ,      vec![0.00, 0.00, 0.00], 0),
+        (02,  None      ,  Some(1.0),      vec![3.33, 0.00, 3.33], 2),
+        (03,  Some(0.51),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (04,  Some(0.52),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (05,  Some(0.53),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (06,  Some(0.54),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (07,  Some(0.55),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (08,  Some(0.56),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (09,  Some(0.57),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (10,  Some(0.58),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (11,  Some(0.59),  None     ,      vec![3.33, 0.00, 3.33], 2),
+        (12,  Some(0.60),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (13,  Some(0.61),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (14,  Some(0.62),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (15,  Some(0.63),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (16,  Some(0.64),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (17,  Some(0.65),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (18,  Some(0.66),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (19,  Some(0.67),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (20,  Some(0.68),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (21,  Some(0.69),  None     ,      vec![6.66, 3.33, 6.66], 5),
+        (22,  Some(0.70),  None     ,      vec![9.99, 3.33, 6.66], 6),
+        (23,  Some(0.71),  None     ,      vec![9.99, 6.66, 6.66], 7),
+        (24,  Some(0.72),  None     ,      vec![9.99, 6.66, 6.66], 7),
     ];
-    let target = test_data.len();
+    let mut target: Vec<f64> = vec![];
+    let mut target_count = 0;
     let conf = ConfTree::new_root(serde_yaml::from_str(r"
         width: 35 mm
         length: 10 m
@@ -72,22 +73,34 @@ fn new() {
         load: point real '/App/Winch.Load'             # in tonn
     ").unwrap());
     let conf = RopeConf::new(&dbg, conf);
-    let result = RefCell::new(0);
+    let result = Rc::new(RefCell::new(vec![0.00, 0.00, 0.00]));
     let mut rope_slices = RopeSlices::new(conf, |ix, deprecation| {
-        let result = result.clone();
         let dbg = &dbg.clone();
         log::debug!("{dbg} | Deprication slice[{ix}]: {:?}", deprecation);
-        *result.borrow_mut() += 1;
+        result.replace_with(|r| {
+            r[ix] += deprecation;
+            r.to_owned()
+        });
     });
-    for (step, pos, load, target) in test_data {
+    for (step, pos, load, target_i, target_count_i) in test_data {
+        target = target_i;
+        target_count = target_count_i;
         log::debug!("{dbg} | step {step}  pos: {:?},  load: {:?}", pos, load);
         let pos = pos.map(|val| val.to_point(0, "pos"));
         let load = load.map(|val| val.to_point(0, "load"));
         let time = Instant::now();
         rope_slices.add(pos, load);
         log::debug!("{dbg} | step {step} elapsed: {:?}", time.elapsed());
-        // assert!(result == target, "{dbg} | step {} \nresult: {:?}\ntarget: {:?}", step, result, target);
+        assert!(
+            result.borrow().iter().enumerate().all(|(ix, r)| {
+                // log::debug!("{dbg} | step {step} result: {}, target: {},  test: {}", r, target[ix], r.trunc_eq(target[ix], 2));
+                r.aprox_eq(target[ix], 1)
+            }),
+            "{dbg} | step {} \nresult: {:?}\ntarget: {:?}", step, result.borrow().to_vec(), target,
+        );
     }
-    assert!(*result.borrow() == target, "{dbg} | \nresult: {:?}\ntarget: {:?}", result.borrow(), target);
+    assert!(result.borrow().len() == target_count, "{dbg} | \nresult: {:?}\ntarget: {:?}", result.borrow().len(), target_count);
+    let result = result.borrow().to_vec();
+    assert!(result == target, "{dbg} | \nresult: {:?}\ntarget: {:?}", result, target);
     test_duration.exit();
 }
