a5107ecd155c2aaf97ad7b9d51eeb1351b8b22b4
Anton Lobanov <a.givertzman@icloud.com>
2025-08-01 17:02:37 +0300

    FrdmService | Api

--- src/infra/api_client.rs
+++ src/infra/api_client.rs
@@ -1,9 +1,8 @@
 use std::sync::{atomic::{AtomicBool, Ordering}, Arc};
-use api_tools::client::api_request::ApiRequest;
+use api_tools::{api::reply::api_reply::ApiReply, client::{api_query::{ApiQuery, ApiQueryKind, ApiQuerySql}, api_request::ApiRequest}, error::api_error::ApiError};
 use sal_core::{dbg::Dbg, error::Error};
-use sal_sync::{services::{entity::{Name, Object}, Service}, sync::Handles, thread_pool::Scheduler};
-
-use crate::infra::ApiClientConf;
+use sal_sync::{services::{entity::{Name, Object}, future::{Future, Sink}, Service}, sync::{channel::{self, RecvTimeoutError}, Handles, Owner}, thread_pool::Scheduler};
+use crate::{domain::{constants::constants::RECV_TIMEOUT, Receiver, Sender}, infra::ApiClientConf};
 
 ///
 /// ## Direct access to the [API-Server](https://github.com/a-givertzman/api-server)
@@ -18,6 +17,8 @@ pub struct ApiClient {
     name: Name,
     conf: ApiClientConf,
     // request: Arc<RwL ApiRequest,
+    send: Sender<(String, Sink<ApiReply>)>,
+    recv: Owner<Receiver<(String, Sink<ApiReply>)>>,
     scheduler: Scheduler,
     handles: Arc<Handles<()>>,
     exit: Arc<AtomicBool>,
@@ -31,19 +32,35 @@ impl ApiClient {
     pub fn new(parent: impl Into<String>, conf: ApiClientConf, scheduler: Scheduler,) -> Self {
         let name = Name::new(parent, "ApiClient");
         let dbg = Dbg::new(name.parent(), name.me());
+        let (send, recv) = channel::unbounded();
         Self {
             name,
             conf,
+            send,
+            recv: Owner::new(recv),
             scheduler,
             handles: Arc::new(Handles::new(&dbg)),
             exit: Arc::new(AtomicBool::new(false)),
             dbg,
         }
     }
-    //
-    //
-    fn exit(&self) {
-        self.exit.store(true, Ordering::Release);
+    ///
+    /// Performs an API request with the parameters specified in the constructor
+    fn fetch(&self, sql: impl Into<String>) -> Future<ApiReply> {
+        let sql = sql.into();
+        let (result, sink) = Future::new();
+        if let Err(err) = self.send.send((sql.clone(), sink.clone())) {
+            sink.add(
+                ApiReply::error(
+                    &self.conf.auth_token,
+                    "not sampled",
+                    true,
+                    sql,
+                    ApiError::new(format!("{}.fetch | Send query error: {}", self.dbg, err), ""),
+                )
+            );
+        }
+        result
     }    
 }
 //
@@ -52,12 +69,63 @@ impl Service for ApiClient {
     fn run(&self) -> Result<(), sal_core::error::Error> {
         log::info!("{}.run | Starting...", self.dbg);
         let dbg = self.dbg.clone();
+        let name = self.name.clone();
         let conf = self.conf.clone();
+        let recv = self.recv.take().unwrap();
         let exit = self.exit.clone();
         let handle = self.scheduler.spawn(move || {
             let dbg = &dbg;
+            let error = Error::new(dbg, "run");
+            let mut request = ApiRequest::new(
+                &name,
+                &conf.address,
+                &conf.auth_token,
+                ApiQuery::new(ApiQueryKind::Sql(ApiQuerySql::new(&conf.database, "")), true),
+                true,
+                false,
+            );
             loop {
-                
+                match recv.recv_timeout(RECV_TIMEOUT) {
+                    Ok((sql, sink)) => {
+                        match request.fetch_with(
+                            &ApiQuery::new(ApiQueryKind::Sql(ApiQuerySql::new(&conf.database, "")), true),
+                            true,
+                        ) {
+                            Ok(reply) => {
+                                match serde_json::from_slice(&reply) {
+                                    Ok(reply) => {
+                                        let reply: ApiReply = reply;
+                                        sink.add(reply);
+                                    }
+                                    Err(err) => sink.add(ApiReply::error(
+                                        &conf.auth_token,
+                                        "not sampled",
+                                        true,
+                                        sql,
+                                        ApiError::new(error.pass_with("Deserialize reply error", err.to_string()).to_string(), ""),
+                                    )),
+                                }
+                            }
+                            Err(err) => sink.add(ApiReply::error(
+                                &conf.auth_token,
+                                "not sampled",
+                                true,
+                                sql,
+                                ApiError::new(error.pass_with("Fetch error", err.to_string()).to_string(), ""),
+                            )),
+                        }
+                    }
+                    Err(err) => match err {
+                        RecvTimeoutError::Timeout => {}
+                        _ => {
+                            log::error!("{dbg}.run | Receive sql error: {:?}", err);
+                            break;
+                        }
+                    }
+                }
+                if exit.load(Ordering::Acquire) {
+                    break;
+                }
             }
             log::info!("{dbg}.run | Exit");
             Ok(())
