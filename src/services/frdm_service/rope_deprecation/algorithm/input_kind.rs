919f256daa23b1aec8581c9ee97bd13baf9e5373
Minyewoo <const.trushov@gmail.com>
2026-03-03 18:58:33 +0300

    release | removed frdm,  added 192103 configs

--- src/services/frdm_service/rope_deprecation/algorithm/input_kind.rs
+++
@@ -1,9 +0,0 @@
-///
-/// Variants of the service input
-/// - Const: ConfDistance
-/// - Point: point real 'App/MultiQueue/Load.MainBoomAngle'
-#[derive(Debug, Clone, PartialEq)]
-pub enum InputKind<T> {
-    Const(T),
-    Point(String),
-}
