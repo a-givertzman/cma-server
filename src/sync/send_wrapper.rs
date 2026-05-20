92963c9fd657807505ea25cd6c19876ccf51db13
Anton Lobanov <a.givertzman@icloud.com>
2026-05-20 14:45:12 +0300

    Task | Fixes

--- src/sync/send_wrapper.rs
+++ src/sync/send_wrapper.rs
@@ -1,40 +1,22 @@
-use std::marker::PhantomData;
-
 ///
 /// Позволяет безопасно пробросить любую структуру в поток.
 /// Даже если внутри структуры Rc / RefCell и подобные синхронные сущности,
 /// которые не имплементят `Send`
-pub struct SendWrapper<T> {
-    ptr: *mut T,
-    _p: PhantomData<T>,
-}
+/// 
+/// **Внимание!** Это можно делать только если структура лоуальная и болностью живет в одном потоке! В противном случае ошибки панмяти!
+pub struct SendWrapper<T>(T);
 //
 impl<T> SendWrapper<T> {
     ///
     /// Заворачиват синхронный тип `T` что бы передать его в поток
     pub fn wrap(v: T) -> Self {
-        Self {
-            // Box::into_raw возвращает чистый указатель *mut T
-            ptr: Box::into_raw(Box::new(v)),
-            _p: PhantomData,
-        }
+        Self(v)
     }
     ///
     /// Извлекает оригинал `T` из обертки
     pub fn extract(self) -> T {
-        // Используем std::mem::ManuallyDrop, чтобы Drop обертки 
-        // случайно не удалил память, которую мы только что извлекли
-        let this = std::mem::ManuallyDrop::new(self);
-        unsafe { *Box::from_raw(this.ptr) }
+        self.0
     }
 }
-// Защита от утечки памяти: если extract() не вызвали, чистим память вручную
-impl<T> Drop for SendWrapper<T> {
-    fn drop(&mut self) {
-        unsafe {
-            let _ = Box::from_raw(self.ptr);
-        }
-    }
-}
-// Что передать в поток
+// Что бы передать в поток
 unsafe impl<T> Send for SendWrapper<T> {}
