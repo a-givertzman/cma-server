adc15a98fca77e95a9744e8bc8ee87a7f87af286
Anton Lobanov <a.givertzman@icloud.com>
2026-07-29 10:29:51 +0300

    VibroMonitor | UdpClient + Tests

--- src/services/vibro_monitor/vibro_monitor.rs
+++ src/services/vibro_monitor/vibro_monitor.rs
@@ -5,6 +5,7 @@ use sal_sync::{
     kernel::state::ExitNotify, services::{Service, Services, entity::{Name, Object}}, thread_pool::Scheduler
 };
 use crate::infra::ApiClient;
+use super::VibroMonitorConf;
 
 ///
 /// VibroMonitor Service | Сервис вибродиагностики
@@ -35,45 +36,46 @@ impl VibroMonitor {
     }
     ///
     /// Stores rope parameters from local settings to thr database
-    fn update_db_settings(&self, winch: usize, api_client: Arc<ApiClient>, exit: Arc<AtomicBool>) -> Result<(), Error> {
-        let dbg = self.dbg.clone();
-        let table = self.conf.table_settings.clone();
-        let rope_length = self.conf.rope_deprecation.crane.rope.length.as_m();
-        let defect_slices = (rope_length / self.conf.rope_defect.segment.as_m()).round() as usize;
-        let deprecation_slices = (rope_length / self.conf.rope_deprecation.crane.rope.segment.as_m()).round() as usize;
-        let _ = self.scheduler.spawn(move || {
-            log::debug!("{dbg}.update_db_settings | Updating db settings...");
-            let sql = format!(r"
-                do $$
-                begin
-                    insert into {table} (id, value) values ('winch{winch}-rope_length', {rope_length})
-                    on conflict (id) do
-                        update set value = {rope_length} where {table}.id = 'winch{winch}-rope_length';
-                    insert into {table} (id, value) values ('winch{winch}-defect_slices', {defect_slices})
-                    on conflict (id) do
-                        update set value = {defect_slices} where {table}.id = 'winch{winch}-defect_slices';
-                    insert into {table} (id, value) values ('winch{winch}-deprecation_slices', {deprecation_slices})
-                    on conflict (id) do
-                        update set value = {deprecation_slices} where {table}.id = 'winch{winch}-deprecation_slices';
-                end; $$
-                language plpgsql;
-            ");
-            log::trace!("{dbg}.update_db_settings | Fetching sql: {:?}", sql);
-            while !exit.load(Ordering::Acquire) {
-                match api_client.fetch(&sql).wait() {
-                    Ok(reply) => {
-                        if reply.is_ok() {
-                            log::debug!("{dbg}.update_db_settings | Updating db settings - Ok {:?}", reply.unwrap());
-                            break;
-                        }
-                        log::warn!("{dbg}.update_db_settings | Sql reply: {:?}", reply);
-                    },
-                    Err(err) => {
-                        log::error!("{dbg}.update_db_settings | Fetch error: {:?}", err);
-                    }
-                }
-            }
-        })?;
+    fn update_db_settings(&self, winch: usize, api_client: Arc<ApiClient>, exit: Arc<ExitNotify>) -> Result<(), Error> {
+        todo!();
+        // let dbg = self.dbg.clone();
+        // let table = self.conf.table_settings.clone();
+        // let rope_length = self.conf.rope_deprecation.crane.rope.length.as_m();
+        // let defect_slices = (rope_length / self.conf.rope_defect.segment.as_m()).round() as usize;
+        // let deprecation_slices = (rope_length / self.conf.rope_deprecation.crane.rope.segment.as_m()).round() as usize;
+        // let _ = self.scheduler.spawn(move || {
+        //     log::debug!("{dbg}.update_db_settings | Updating db settings...");
+        //     let sql = format!(r"
+        //         do $$
+        //         begin
+        //             insert into {table} (id, value) values ('winch{winch}-rope_length', {rope_length})
+        //             on conflict (id) do
+        //                 update set value = {rope_length} where {table}.id = 'winch{winch}-rope_length';
+        //             insert into {table} (id, value) values ('winch{winch}-defect_slices', {defect_slices})
+        //             on conflict (id) do
+        //                 update set value = {defect_slices} where {table}.id = 'winch{winch}-defect_slices';
+        //             insert into {table} (id, value) values ('winch{winch}-deprecation_slices', {deprecation_slices})
+        //             on conflict (id) do
+        //                 update set value = {deprecation_slices} where {table}.id = 'winch{winch}-deprecation_slices';
+        //         end; $$
+        //         language plpgsql;
+        //     ");
+        //     log::trace!("{dbg}.update_db_settings | Fetching sql: {:?}", sql);
+        //     while !exit.load(Ordering::Acquire) {
+        //         match api_client.fetch(&sql).wait() {
+        //             Ok(reply) => {
+        //                 if reply.is_ok() {
+        //                     log::debug!("{dbg}.update_db_settings | Updating db settings - Ok {:?}", reply.unwrap());
+        //                     break;
+        //                 }
+        //                 log::warn!("{dbg}.update_db_settings | Sql reply: {:?}", reply);
+        //             },
+        //             Err(err) => {
+        //                 log::error!("{dbg}.update_db_settings | Fetch error: {:?}", err);
+        //             }
+        //         }
+        //     }
+        // })?;
         Ok(())
     }
 }
@@ -113,9 +115,9 @@ impl Service for VibroMonitor {
                 .map(|(_, ch)| ch)
                 .collect::<String>()
         );
-        if let Err(err) = self.create_rope_defects_dir(&storage_path) {
-            log::warn!("{}.run | Can't create folder for rope defects images: {:?}", self.dbg, err);
-        }
+        // if let Err(err) = self.create_rope_defects_dir(&storage_path) {
+        //     log::warn!("{}.run | Can't create folder for rope defects images: {:?}", self.dbg, err);
+        // }
         let api_client = Arc::new(ApiClient::new(conf.api.clone(), scheduler.clone()));
         self.tasks.insert(api_client.name().join(), api_client.clone());
         api_client.run()?;
@@ -140,38 +142,38 @@ impl Service for VibroMonitor {
         //     })
         //     .collect();
         // let (_, recv) = services.subscribe(&conf.subscribe, &name.join(), &subscription);
-        let inputs = Arc::new(Inputs::new(&name, &conf, services.clone(), scheduler.clone(), self.exit.clone()));
-        self.tasks.insert(inputs.name().join(), inputs.clone());
-        let rope_deprecation = Arc::new(RopeDeprecation::new(
-            &self.name,
-            conf.rope_deprecation,
-            inputs.clone(),
-            api_client.clone(),
-            scheduler.clone(),
-        ));
-        self.tasks.insert(rope_deprecation.name().join(), rope_deprecation.clone());
-        rope_deprecation.run()?;
-        log::info!("{}.run | RopeDeprecation ready", self.dbg);
-        if !conf.rope_defect.cameras.is_empty() {
-            log::info!("{}.run | Camera's configured: {}", self.dbg, conf.rope_defect.cameras.len());
-            for (camera_id, camera_conf) in &conf.rope_defect.cameras {
-                log::info!("{}.run | Camera '{}' [{}]", self.dbg, camera_conf.name, **camera_id);
-                let defect_detection = Arc::new(RopeDefect::new(
-                    &self.name,
-                    conf.rope_defect.clone(),
-                    **camera_id,
-                    storage_path.clone(),
-                    inputs.clone(),
-                    api_client.clone(),
-                    scheduler.clone(),
-                ));
-                self.tasks.insert(defect_detection.name().join(), defect_detection.clone());
-                defect_detection.run()?; 
-            }
-        } else {
-            log::warn!("{}.run | No Camera's configured", self.dbg);
-        }
-        inputs.run()?;      // have to be started after all subscription being added, then it will subscribe all them on MultiQueue
+        // let inputs = Arc::new(Inputs::new(&name, &conf, services.clone(), scheduler.clone(), self.exit.clone()));
+        // self.tasks.insert(inputs.name().join(), inputs.clone());
+        // let rope_deprecation = Arc::new(RopeDeprecation::new(
+        //     &self.name,
+        //     conf.rope_deprecation,
+        //     inputs.clone(),
+        //     api_client.clone(),
+        //     scheduler.clone(),
+        // ));
+        // self.tasks.insert(rope_deprecation.name().join(), rope_deprecation.clone());
+        // rope_deprecation.run()?;
+        // log::info!("{}.run | RopeDeprecation ready", self.dbg);
+        // if !conf.rope_defect.cameras.is_empty() {
+        //     log::info!("{}.run | Camera's configured: {}", self.dbg, conf.rope_defect.cameras.len());
+        //     for (camera_id, camera_conf) in &conf.rope_defect.cameras {
+        //         log::info!("{}.run | Camera '{}' [{}]", self.dbg, camera_conf.name, **camera_id);
+        //         let defect_detection = Arc::new(RopeDefect::new(
+        //             &self.name,
+        //             conf.rope_defect.clone(),
+        //             **camera_id,
+        //             storage_path.clone(),
+        //             inputs.clone(),
+        //             api_client.clone(),
+        //             scheduler.clone(),
+        //         ));
+        //         self.tasks.insert(defect_detection.name().join(), defect_detection.clone());
+        //         defect_detection.run()?; 
+        //     }
+        // } else {
+        //     log::warn!("{}.run | No Camera's configured", self.dbg);
+        // }
+        // inputs.run()?;      // have to be started after all subscription being added, then it will subscribe all them on MultiQueue
         log::info!("{}.run | RopeDefect's ready", self.dbg);
         log::info!("{}.run | Starting - Ok", self.dbg);
         Ok(())
@@ -207,7 +209,7 @@ impl Service for VibroMonitor {
     //
     //
     fn exit(&self) {
-        self.exit.store(true, Ordering::Release);
+        self.exit.exit();
         for task in self.tasks.iter() {
             task.value().exit();
         }
